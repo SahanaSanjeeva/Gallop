a0986c41a33657d4ca761fb88c95b7f93f16cc59
SahanaSanjeeva <spsahana@gmail.com>
2017-06-02 22:09:21 -0500

    Updated changes as per client requirements to all pages

--- GallopNYC/ProgramsViewController.swift
+++ GallopNYC/ProgramsViewController.swift
@@ -10,14 +10,15 @@ import UIKit
 
 class ProgramsViewController: UIViewController {
     
-    @IBOutlet var jobSkillYT: UIWebView!
+    //@IBOutlet var jobSkillYT: UIWebView!
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let jobSkillYTUrl = "https://www.youtube.com/embed/4JVuWvOn30I"
+        //let jobSkillYTUrl = "https://www.youtube.com/embed/4JVuWvOn30I"
         
-        jobSkillYT.allowsInlineMediaPlayback = true
-        jobSkillYT.loadHTMLString("<iframe width=\"316\" height=\"144\" src=\"\(jobSkillYTUrl)/?&playsinline=1\" frameborder=\"0\" allowfullscreen></iframe>", baseURL: nil)    }
+        //jobSkillYT.allowsInlineMediaPlayback = true
+        //jobSkillYT.loadHTMLString("<iframe width=\"316\" height=\"144\" src=\"\(jobSkillYTUrl)/?&playsinline=1\" frameborder=\"0\" allowfullscreen></iframe>", baseURL: nil)    
+    }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
